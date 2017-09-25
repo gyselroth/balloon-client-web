fa590578b08330f6bbb55eb4d26f6f6346f1f74a
Raffael Sahli <sahli@gyselroth.com>
2017-09-25 09:46:26 +0200

    fixes dev webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,11 +4,12 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   devtool: 'eval-source-map',
   devServer: {
-  proxy: {
-    '/api': {
-      target: process.env.BALLOON_API_URL || 'http://localhost:8081',
-      changeOrigin: true,
-      secure: (process.env.BALLOON_API_URL_INSECURE || 'false') !== 'true'
+    proxy: {
+      '/api': {
+        target: process.env.BALLOON_API_URL || 'http://localhost:8081',
+        changeOrigin: true,
+        secure: (process.env.BALLOON_API_URL_INSECURE || 'false') !== 'true'
+      }
     }
   }
 });
