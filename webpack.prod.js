e3f98586f100101396f151994c55129b43372655
Raffael Sahli <sahli@gyselroth.com>
2017-10-12 14:18:01 +0200

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,34 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-});
+console.log(common);
+common.module.rules.unshift(      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+)
+
+module.exports = common;
+
+/*module.exports = merge(common, {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+});*/
