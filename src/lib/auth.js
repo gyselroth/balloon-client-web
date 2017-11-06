64df402f155dcaf5ee9dffb18a22e0dcc7f4208d
Stefan Aebischer <aebischer@pixtron.ch>
2017-11-06 13:26:14 -0300

    Adapt auth.js for balloon server 1.0.16

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,3 @@
-/**
- * Balloon
- *
- * @author    Raffael Sahli <sahli@gyselroth.net>
- * @copyright Copryright (c) 2012-2017 gyselroth GmbH (https://gyselroth.com)
- * @license   GPL-3.0 https://opensource.org/licenses/GPL-3.0
- */
-
 import * as $ from "jquery";
 import balloon from './core.js';
 const {AuthorizationServiceConfiguration} = require('@openid/appauth/built/authorization_service_configuration.js');
@@ -14,325 +6,262 @@ const {AuthorizationNotifier} = require('@openid/appauth/built/authorization_req
 const {RedirectRequestHandler} = require('@openid/appauth/built/redirect_based_handler.js');
 
 var login = {
-  token: undefined,
-  adapter: null,
-  username: null,
-  basic: true,
-  oidc: [],
-  notifier: null,
-  handler: null,
+    token: {},
+    oauth2: false,
+    adapter: null,
+    username: null,
 
-  init: function(config) {
-    if(config && config.auth) {
-      if(config.auth.basic === false) {
-        this.basic = false;
-      }
+    init: function() {
+        /*JSO.enablejQuery(jQuery);
+        if(typeof(config) === 'object' && config.oauth2 === true) {
+            //this breaks OAUTH2
+            //config.jso.redirect_uri += location.search;
+            login.oauth2 = new JSO(config.jso);
 
-      if(config.auth.oidc) {
-        this.oidc = config.auth.oidc;
-      }
-    }
-
-    if(this.basic === false) {
-      $('#login-basic').hide();
-    }
-
-    var $oidc = $('#login-oidc');
-    for(let i in this.oidc) {
-      $oidc.append('<img alt="'+this.oidc[i].providerUrl+'" src="data:image/png;base64,'+this.oidc[i].imgBase64+'" />');
-    }
-
-    this.checkAuth();
-  },
-
-  parseAuthorizationResponse: function() {
-    var hash = window.location.hash.substr(1);
-    var obj = {};
-    var pairs = hash.split('&');
-
-    for(let i in pairs){
-      let split = pairs[i].split('=');
-      obj[decodeURIComponent(split[0])] = decodeURIComponent(split[1]);
-    }
-
-    return obj;
-  },
-
-  checkOidcAuth: function() {
-    this.notifier = new AuthorizationNotifier();
-    this.handler = new RedirectRequestHandler();
-
-    this.notifier.setAuthorizationListener(function (request, response, error) {
-      var hash = login.parseAuthorizationResponse();
-      if (response && hash.access_token) {
-        login.token = hash.access_token;
-        login.adapter = 'oidc';
-        login.verifyOidcAuthentication();
-      } else {
-        $('#login-oidc-error').show();
-      }
-
-      window.location.hash = '';
-    });
-
-    this.handler.setAuthorizationNotifier(this.notifier);
-    this.handler.completeAuthorizationRequestIfPossible();
-  },
-
-  checkAuth: function() {
-    this.checkOidcAuth();
-
-    var login_helper = function(e) {
-      login.destroyBrowser();
-      var $login = $('#login').show();
-      $('#fs-namespace').hide();
-      $login.find('input[type=submit]').off('click').on('click', login.initBasicAuth);
-
-      if(localStorage.username !== undefined) {
-        $login.find('input[name=username]').val(localStorage.username);
-      }
-
-      $(document).on('keydown', function(e) {
-        if(e.keyCode === 13 && login.basic === true) {
-          login.initBasicAuth();
-        }
-      });
-
-      $('#login-oidc').find('img').off('click').on('click', function() {
-        $login.find('.error-message').hide();
-        login.initOidcAuth($(this).attr('alt'));
-      });
-    };
-
-    var options = {
-      type:'GET',
-      url: '/api/auth',
-      cache: false,
-      complete: function(response) {
-        switch(response.status) {
-          case 401:
-          case 403:
-            login_helper(response);
-          break;
-
-          case 400:
-            if(login.token) {
-              login.adapter = 'oidc';
-            } else {
-              login.adapter = 'basic';
+            if(config.oauth2_login_button !== undefined) {
+                $('#login-oauth').show().html('<img src="'+config.oauth2_login_button+'"/>');
             }
+        }*/
 
-            login.fetchIdentity();
-          break;
+        this.checkAuth();
+    },
 
-          default:
-            $('#login').show();
-            $('#login-server-error').show();
-            $('#login-body').hide();
-          break;
+    isOauth2Enabled: function() {
+        if(typeof(login.oauth2) === 'object') {
+            return true;
         }
-      }
-    }
-
-    if(login.token) {
-      options.headers = {
-        "Authorization": 'Bearer '+login.token,
-      }
-    }
-
-    $.ajax(options);
-  },
-
-  logout: function() {
-    login.token = null;
-    login.destroyBrowser();
-
-    if(login.adapter === 'basic') {
-      if(navigator.userAgent.indexOf('MSIE') > -1 || navigator.userAgent.indexOf('Edge') > -1) {
-        document.execCommand('ClearAuthenticationCache', 'false');
-        login.destroyBrowser();
-        login.checkAuth();
-      } else {
-        $.ajax({
-          url: '/api/v'+balloon.BALLOON_API_VERSION,
-          username: '_logout',
-          password: 'logout',
-          cache: false,
-          statusCode: {
-            401: function() {
-              login.destroyBrowser();
-              login.checkAuth();
-            }
-          }
-        });
-      }
-    }
-  },
-
-  verifyOidcAuthentication: function() {
-    login.xmlHttpRequest({
-      url: '/api/auth',
-      cache: false,
-      complete: function(response) {
-        switch(response.status) {
-          case 401:
-          case 403:
-            $('#login-oidc-error').show();
-          break;
-
-          case 400:
-            login.fetchIdentity();
-            login.initBrowser();
-          break;
-
-          default:
-            $('#login').show();
-            $('#login-server-error').show();
-            $('#login-body').hide();
-          break;
+        else {
+            return false;
         }
-      }
-    });
-  },
+    },
 
-  fetchIdentity: function() {
-    login.xmlHttpRequest({
-      url: '/api/v'+balloon.BALLOON_API_VERSION+'/user/whoami',
-      dataType: 'json',
-      cache: false,
-      success: function(body) {
-        login.username = body.data;
-        localStorage.username = login.username;
-        $('#fs-identity').show().find('#fs-identity-username').html(body.data);
-      }
-    });
-  },
+    checkAuth: function() {
+        if(login.isOauth2Enabled()) {
+            login.oauth2.callback(null, function(token) {
+                login.token = token;
+            });
+        }
 
-  getUsername: function() {
-    return this.username;
-  },
-
-  getAccessToken: function() {
-    return this.token;
-  },
-
-  xmlHttpRequest: function(options) {
-    if(login.token) {
-      if(options.headers) {
-          options.headers["Authorization"] = 'Bearer '+login.token;
-      } else {
-          options.headers = {
-            "Authorization": 'Bearer '+login.token
-          };
-      }
-    }
-
-    return $.ajax(options);
-  },
-
-  getIdpConfigByProviderUrl: function(provider_url) {
-    for(let i in this.oidc) {
-      if(this.oidc[i].providerUrl === provider_url) {
-        return this.oidc[i];
-      }
-    }
-  },
-
-  initOidcAuth: function(provider_url) {
-    var idp = this.getIdpConfigByProviderUrl(provider_url);
-
-    AuthorizationServiceConfiguration.fetchFromIssuer(idp.providerUrl).then(configuration => {
-      var request = new AuthorizationRequest(
-        idp.clientId, idp.redirectUri, idp.scope, 'id_token token', undefined, {'nonce': Math.random().toString(36).slice(2)});
-
-      login.handler.performAuthorizationRequest(configuration, request);
-    });
-  },
-
-  initBasicAuth: function() {
-    var $login = $('#login');
-    $login.find('.error-message').hide();
-    var $username_input = $login.find('input[type=text]');
-    var $password_input = $login.find('input[type=password]');
-
-    var username = $username_input.val();
-    var password = $password_input.val();
-
-    if(username == '') {
-      $username_input.addClass('error');
-    }
-    if(password == '') {
-      $password_input.addClass('error');
-    }
-
-    if(username == '' || password == '') {
-      return;
-    }
-
-    $password_input.val('');
-
-    login.doBasicAuth(username, password);
-  },
-
-  doBasicAuth: function(username, password) {
-    var $login = $('#login');
-    var $username_input = $login.find('input[type=text]');
-    var $password_input = $login.find('input[type=password]');
-
-    $.ajax({
-      type: 'GET',
-      username: username,
-      password: password,
-      dataType: 'json',
-      url: '/api/v1/user/whoami',
-      beforeSend: function() {
-        $username_input.removeClass('error');
-        $password_input.removeClass('error');
-      },
-      complete: function(response) {
-        switch(response.status) {
-          case 401:
-          case 403:
+        var login_helper = function(e) {
+            login.destroyAccessToken();
+            login.destroyBrowser();
+            var $login = $('#login').show();
             $('#fs-namespace').hide();
-            $('#login-basic-error').show();
-            $username_input.addClass('error');
-            $password_input.addClass('error');
-          break;
+            $('#login').find('input[type=submit]').off('click').on('click', login.initBasicAuth);
 
-          case 200:
-            login.adapter = 'basic';
-            login.username = response.responseJSON.data;
-            localStorage.username = login.username;
-            $('#fs-identity').show().find('#fs-identity-username').html(login.username);
-            login.initBrowser();
-          break;
+            if(localStorage.username !== undefined) {
+                $login.find('input[name=username]').val(localStorage.username);
+            }
 
-          default:
-            $('#login').show();
-            $('#login-server-error').show();
-            $('#login-body').hide();
-          break;
+            $(document).on('keydown', function(e) {
+                if(e.keyCode === 13) {
+                    login.initBasicAuth();
+                }
+            });
+
+            if(login.isOauth2Enabled()) {
+                $('#login-oauth').off('click').on('click', login.initOauth2);
+            }
+        };
+
+        var options = {
+            type:'GET',
+            url: '/api/auth',
+            statusCode: {
+                500: function(e) {
+                    balloon.displayError(e);
+                },
+                401: login_helper,
+                403: login_helper,
+                400: function(e) {
+                    if(login.getAccessToken() !== false) {
+                        login.adapter = 'oauth2';
+                    }
+                    else {
+                        login.adapter = 'basic';
+                    }
+
+                    login.setIdentity();
+                    login.initBrowser();
+                }
+            }
         }
-      }
-    });
-  },
 
-  initBrowser: function() {
-    $('#login').hide();
-    $('#fs-namespace').show();
+        if(login.getAccessToken() != '') {
+            options.headers = {
+                "Authorization": 'Bearer '+login.getAccessToken(),
+            }
+        }
 
-    $('#fs-menu-user-logout').unbind('click').bind('click', function() {
-      login.logout();
-    });
+        $.ajax(options);
+    },
 
-    balloon.init();
-  },
+    logout: function() {
+        if(login.adapter == 'oauth2') {
+            $.ajax({
+                type: 'post',
+                cache: false,
+                url: config.jso.revoke+'?access_token='+login.getAccessToken(),
+                complete: function() {
+                    login.destroyAccessToken();
+                    login.destroyBrowser();
+                    window.history.pushState('Login', 'Title', '/');
+                    login.checkAuth();
+                }
+            });
+        }
+        else {
+            if(navigator.userAgent.indexOf('MSIE') > -1 || navigator.userAgent.indexOf('Edge') > -1) {
+                document.execCommand('ClearAuthenticationCache', 'false');
+                login.destroyBrowser();
+                login.checkAuth();
+            }
+            else {
+                $.ajax({
+                    url: '/api/v'+balloon.BALLOON_API_VERSION,
+                    username: '_logout',
+                    password: 'logout',
+                    cache: false,
+                    statusCode: {
+                        401: function() {
+                            login.destroyBrowser();
+                            login.checkAuth();
+                        }
+                    }
+                });
+            }
+        }
+    },
 
-  destroyBrowser: function() {
-    $('#login').show();
-    balloon.resetDom();
-    $('#fs-namespace').hide();
-  },
+    setIdentity: function() {
+        var headers = {};
+
+        if(login.getAccessToken() !== false) {
+            headers = {
+                "Authorization": 'Bearer '+login.getAccessToken(),
+            };
+        }
+
+        $.ajax({
+            url: '/api/v'+balloon.BALLOON_API_VERSION+'/user/whoami',
+            headers: headers,
+            cache: false,
+            success: function(body) {
+                login.username = body.data;
+                localStorage.username = login.username;
+                $('#fs-identity').show().find('#fs-identity-username').html(body.data);
+
+                $('#fs-menu-user-logout').unbind('click').bind('click', function() {
+                    login.logout();
+                });
+            }
+        });
+    },
+
+    getUsername: function() {
+        return login.username;
+    },
+
+    destroyAccessToken: function() {
+        /*if(config.oauth2 === true) {
+            login.oauth2.wipeTokens();
+        }*/
+
+        login.token = {};
+    },
+
+    getAccessToken: function()
+    {
+        if(login.token.access_token === undefined) {
+            return false;
+        }
+        else {
+            return login.token.access_token;
+        }
+    },
+
+    xmlHttpRequest: function(options) {
+        if(login.getAccessToken() !== false) {
+            return login.oauth2.ajax(options);
+        }
+        else {
+            return $.ajax(options);
+        }
+    },
+
+    initOauth2: function() {
+        login.oauth2.getToken(function(token){login.token = token;});
+        login.checkAuth();
+    },
+
+    initBasicAuth: function() {
+        var username_input = $('#login').find('input[type=text]');
+        var password_input = $('#login').find('input[type=password]');
+
+        var username = username_input.val();
+        var password = password_input.val();
+
+        if(username == '') {
+            username_input.addClass('error');
+        }
+        if(password == '') {
+            password_input.addClass('error');
+        }
+
+        if(username == '' || password == '') {
+            return;
+        }
+
+        password_input.val('');
+
+        login.doAuth(username, password);
+    },
+
+    doAuth: function(username, password) {
+        var username_input = $('#login').find('input[type=text]');
+        var password_input = $('#login').find('input[type=password]');
+
+        $.ajax({
+            type: 'GET',
+            username: username,
+            password: password,
+            url: '/api/v1',
+            beforeSend: function() {
+                username_input.removeClass('error');
+                password_input.removeClass('error');
+            },
+            statusCode: {
+                401: function() {
+                    username_input.addClass('error');
+                    password_input.addClass('error');
+                },
+                403: function() {
+                    $('#fs-namespace').hide();
+                    username_input.addClass('error');
+                    password_input.addClass('error');
+                },
+                200: function() {
+                    login.basic_auth =
+                    username_input.removeClass('error');
+                    password_input.removeClass('error');
+                    login.adapter = 'basic';
+                    login.initBrowser();
+                    login.setIdentity();
+                }
+            }
+        });
+    },
+
+    initBrowser: function() {
+        $('#login').hide();
+        $('#fs-namespace').show();
+        balloon.init();
+    },
+
+    destroyBrowser: function() {
+        $('#login').show();
+        balloon.resetDom();
+        $('#fs-namespace').hide();
+    },
 }
-
 export default login;
