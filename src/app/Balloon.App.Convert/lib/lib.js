cf703780c08d1af0327011bf40a9643fc2633213
Raffael Sahli <sahli@gyselroth.com>
2018-01-09 11:47:03 +0100

    happy new year

--- src/app/Balloon.App.Convert/lib/lib.js
+++ src/app/Balloon.App.Convert/lib/lib.js
@@ -2,7 +2,7 @@
  * Balloon
  *
  * @author      Raffael Sahli <sahli@gyselroth.net>
- * @copyright   Copryright (c) 2012-2017 gyselroth GmbH (https://gyselroth.com)
+ * @copyright   Copryright (c) 2012-2018 gyselroth GmbH (https://gyselroth.com)
  * @license     GPLv3 https://opensource.org/licenses/GPL-3.0
  */
 import $ from "jquery";
--- src/lib/core.js
+++ src/lib/core.js
@@ -1,7 +1,7 @@
 /**
  * balloon
  *
- * @copyright Copryright (c) 2012-2017 gyselroth GmbH (https://gyselroth.com)
+ * @copyright Copryright (c) 2012-2018 gyselroth GmbH (https://gyselroth.com)
  * @license   GPL-3.0 https://opensource.org/licenses/GPL-3.0
  */
 
