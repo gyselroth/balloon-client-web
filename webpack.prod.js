a813d611f56a4d8f3177e2eee6b72abd01372c62
Raffael Sahli <sahli@gyselroth.com>
2018-03-05 11:16:41 +0100

    fixes 1.x build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 common.module.rules.unshift({
         test: /\.js$/,
@@ -13,31 +12,6 @@ common.module.rules.unshift({
           }
         }
       }
-)
-
-common.plugins.push(
-  new FaviconsWebpackPlugin({
-    logo: './themes/default/img/icon_blue.svg',
-    prefix: 'icons-[hash]/',
-    emitStats: false,
-    statsFilename: 'iconstats-[hash].json',
-    persistentCache: true,
-    inject: true,
-    background: 'transparent',
-    title: 'balloon web ui',
-    icons: {
-      android: true,
-      appleIcon: true,
-      appleStartup: true,
-      coast: false,
-      favicons: true,
-      firefox: true,
-      opengraph: true,
-      twitter: false,
-      yandex: false,
-      windows: true
-    }
-  })
 )
 
 module.exports = common;
