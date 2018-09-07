05a5e87c7430b3961b2c5866753b374663803140
Stefan Aebischer <aebischer@pixtron.ch>
2018-09-07 15:44:25 +0200

    Add "delete all" displayed notifications

--- src/app/Balloon.App.Notification/lib/lib.js
+++ src/app/Balloon.App.Notification/lib/lib.js
@@ -12,7 +12,7 @@ import css from '../styles/style.scss';
 var app = {
   id: 'Balloon.App.Notification',
   _loadingMessages: false,
-  _messagesLimit: 3,
+  _messagesLimit: 10,
 
   preInit: function(core) {
     this.balloon = core;
@@ -44,6 +44,7 @@ var app = {
           '<div id="fs-notifications-dropdown" class="bln-dropdown-content">'+
             '<div id="fs-notifications-header">'+
               '<h3>'+ i18next.t('app.balloon_app_notification.messages.title') +'</h3>'+
+              '<button id="fs-notifications-delete-all" class="fs-button-primary">' + i18next.t('app.balloon_app_notification.messages.delete_all') + '</button>'+
             '</div>'+
             '<div id="fs-notifications-messages-wrap">'+
               '<div id="fs-notifications-no-messages">'+ i18next.t('app.balloon_app_notification.messages.no_messages') +'</div>'+
@@ -59,6 +60,7 @@ var app = {
     this.$contentMessagesDropdown = this.$contentMessages.find('#fs-notifications-dropdown');
     this.$contentMessagesContainer = this.$contentMessages.find('#fs-notifications-messages');
     this.$contentMessagesCount = this.$contentMessages.find('#fs-notifications-count');
+    this.$contentMessagesDeleteAll = this.$contentMessages.find('#fs-notifications-delete-all');
   },
 
   /**
@@ -75,9 +77,15 @@ var app = {
       $list.data('fsNotificationsOffset', 0);
 
       app.$contentMessagesDropdown.removeClass('has-messages');
+      app.$contentMessagesDeleteAll.off('click').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-      app._displayMessages().done(function() {
-        app._displayMessagesInfiniteScroll();
+        app._deleteAllDisplayedMessages();
+      });
+
+      app._displayMessages().done(function(body) {
+        //app._displayMessagesInfiniteScroll();
       });
     });
   },
@@ -95,6 +103,53 @@ var app = {
     var limit = limit || app._messagesLimit;
     var offset = offset || $list.data('fsNotificationsOffset');
 
+    var request = app._fetchMessages(offset, limit);
+
+    $list.data('fsNotificationsOffset', (offset + limit))
+
+    request.done(function(body) {
+      $list.data('fsNotificationsTotalMessages', body.total);
+
+      if(body.total > 0) {
+        app.$contentMessagesCount.html(body.total).show();
+        app.$contentMessagesDropdown.addClass('has-messages');
+
+        var messages = body.data;
+
+        for(var i=0; i<messages.length; i++) {
+          var message = messages[i];
+
+          var $message = $(
+            '<li data-fs-message-id="' + message.id + '">'+
+              '<p class="fs-notifications-meta">' + i18next.t('app.balloon_app_notification.messages.message_meta', message.sender.username) + '</p>'+
+              '<h4>' + message.subject + '</h4>'+
+              '<p>' + message.message + '</p>'+
+              '<div class="fs-notifications-delete-message"><svg class="gr-icon gr-i-close" viewBox="0 0 24 24"><use xlink:href="/assets/icons.svg#close"></use></svg></div>'+
+            '</li>'
+          );
+
+          $message.find('.fs-notifications-delete-message').click(app._onDeleteMessage);
+          $list.append($message);
+        }
+      } else {
+        app.$contentMessagesDropdown.removeClass('has-messages');
+        app.$contentMessagesCount.html(0).hide();
+      }
+
+      app._loadingMessages = false;
+    });
+
+    return request;
+  },
+
+  /**
+   * Delete all messages
+   *
+   * @param int offset
+   * @param int limit
+   * @return void
+   */
+  _fetchMessages: function(offset, limit) {
     return app.balloon.xmlHttpRequest({
       url: app.balloon.base+'/notifications',
       type: 'GET',
@@ -103,36 +158,6 @@ var app = {
         limit: limit,
         offset: offset
       },
-      success: function(body) {
-        $list.data('fsNotificationsOffset', (offset + limit))
-        $list.data('fsNotificationsTotalMessages', body.total);
-
-        if(body.total > 0) {
-          app.$contentMessagesCount.html(body.total).show();
-          app.$contentMessagesDropdown.addClass('has-messages');
-          var messages = body.data;
-          for(var i=0; i<messages.length; i++) {
-            var message = messages[i];
-
-            var $message = $(
-              '<li data-fs-message-id="' + message.id + '">'+
-                '<p class="fs-notifications-meta">' + i18next.t('app.balloon_app_notification.messages.message_meta', message.sender.username) + '</p>'+
-                '<h4>' + message.subject + '</h4>'+
-                '<p>' + message.message + '</p>'+
-                '<div class="fs-notifications-delete-message"><svg class="gr-icon gr-i-close" viewBox="0 0 24 24"><use xlink:href="/assets/icons.svg#close"></use></svg></div>'+
-              '</li>'
-            );
-
-            $message.find('.fs-notifications-delete-message').click(app._deleteMessage);
-            $list.append($message);
-          }
-        } else {
-          app.$contentMessagesDropdown.removeClass('has-messages');
-          app.$contentMessagesCount.html(0).hide();
-        }
-
-        app._loadingMessages = false;
-      }
     });
   },
 
@@ -159,17 +184,52 @@ var app = {
   },
 
   /**
-   * Click callback, deleting a given message
+   * Delete all messages
    *
    * @return void
    */
-  _deleteMessage: function(event) {
-    event.stopPropagation();
+  _deleteAllDisplayedMessages: function() {
+    var $messages = app.$contentMessagesContainer.find('li');
+    var messageIds = [];
 
-    var $message = $(event.target).closest('li');
-    var messageId = $message.data('fsMessageId');
+    $messages.each(function(index, message) {
+      messageIds.push($(message).data('fsMessageId'));
+    });
 
-    return app.balloon.xmlHttpRequest({
+    app._deleteMessagesIds(messageIds);
+  },
+
+  /**
+   * Delete given messages
+   *
+   * @param array messageIds
+   * @return void
+   */
+  _deleteMessagesIds: function(messageIds) {
+    var requests = messageIds.map(function(messageId) {
+      return app._deleteMessage(messageId, false);
+    });
+
+    var $d = $.when.apply($, requests);
+
+    $d.always(function() {
+      app._displayMessages();
+    });
+
+    return $d;
+  },
+
+  /**
+   * Delete a given message
+   *
+   * @param string messageId
+   * @param boolean loadNext
+   * @return void
+   */
+  _deleteMessage: function(messageId, loadNext) {
+    var $d = $.Deferred();
+
+    app.balloon.xmlHttpRequest({
       url: app.balloon.base+'/notifications',
       type: 'DELETE',
       dataType: 'json',
@@ -177,6 +237,7 @@ var app = {
         id: messageId
       },
       success: function(body) {
+        var $message = app.$contentMessagesContainer.find('li[data-fs-message-id="' + messageId + '"]');
         var $list = app.$contentMessagesContainer;
         var offset = $list.data('fsNotificationsOffset');
         var total = $list.data('fsNotificationsTotalMessages');
@@ -189,7 +250,7 @@ var app = {
         if(total > 0) {
           app.$contentMessagesCount.html(total).show();
 
-          if(offset < total) {
+          if(loadNext && offset < total) {
             //load next message
             app._displayMessages(offset, 1);
           }
@@ -199,8 +260,26 @@ var app = {
         }
 
         $message.remove();
+        $d.resolve();
+      },
+      error: function() {
+        $d.reject();
       }
     });
+
+    return $d;
+  },
+
+  /**
+   * Click callback, deleting a given message
+   *
+   * @return void
+   */
+  _onDeleteMessage: function(event) {
+    event.stopPropagation();
+
+    var messageId = $(event.target).closest('li').data('fsMessageId');
+    app._deleteMessage(messageId, true);
   },
 
   /** Settings **/
