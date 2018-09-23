gyselroth/balloon-client-web

commit 7b4c2b3979e5492fec3675a798b130304e16a117

Escape message input, which might be user generated

src/app/Balloon.App.Notification/lib/lib.js
@@ -130,18 +130,24 @@ var app = {
             meta = i18next.t('app.notification.messages.message_meta_no_sender', date);
           }
 
-          var $message = $(
-            '<li data-fs-message-id="' + message.id + '">'+
-              '<div class="fs-notifications-message-inner">'+
-                '<p class="fs-notifications-meta">' + meta + '</p>'+
-                '<h4>' + message.subject + '</h4>'+
-                '<p>' + message.message + '</p>'+
-                '<div class="fs-notifications-delete-message"><svg class="gr-icon gr-i-close" viewBox="0 0 24 24"><use xlink:href="/assets/icons.svg#close"></use></svg></div>'+
-              '</div>'+
-            '</li>'
+          var $messageContent = $('<div class="fs-notifications-message-inner"></div>');
+
+          $messageContent.append('<p class="fs-notifications-meta"></p>').text(meta);
+          $messageContent.append('<h4></h4>').text(message.subject);
+          $messageContent.append('<p></p>').text(message.message);
+          $messageContent.append(
+            '<div class="fs-notifications-delete-message">'+
+              '<svg class="gr-icon gr-i-close" viewBox="0 0 24 24">'+
+                '<use xlink:href="/assets/icons.svg#close"></use>'+
+              '</svg>'+
+            '</div>'
           );
 
-          $message.find('.fs-notifications-delete-message').click(app._onDeleteMessage);
+          $messageContent.find('.fs-notifications-delete-message').click(app._onDeleteMessage);
+
+          var $message = $('<li data-fs-message-id="' + message.id + '"></li>');
+
+          $message.append($messageContent);
           $list.append($message);
         }
       } else {

commit 7b1280b0b20d378e2f748c8b7492db8ac91e5c70

Fix regression
Fixes a regression introduced with 7b4c2b3, which removed parts of notifications

src/app/Balloon.App.Notification/lib/lib.js
@@ -132,9 +132,9 @@ var app = {
 
           var $messageContent = $('<div class="fs-notifications-message-inner"></div>');
 
-          $messageContent.append('<p class="fs-notifications-meta"></p>').text(meta);
-          $messageContent.append('<h4></h4>').text(message.subject);
-          $messageContent.append('<p></p>').text(message.message);
+          $messageContent.append($('<p class="fs-notifications-meta"></p>').text(meta));
+          $messageContent.append($('<h4></h4>').text(message.subject));
+          $messageContent.append($('<p></p>').text(message.message));
           $messageContent.append(
             '<div class="fs-notifications-delete-message">'+
               '<svg class="gr-icon gr-i-close" viewBox="0 0 24 24">'+