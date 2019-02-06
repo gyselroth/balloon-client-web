c58d2fda176b8ece01f6e46841d77fed521f1076
Stefan Aebischer <aebischer@pixtron.ch>
2019-02-06 15:37:56 +0200

    Add intelligent collection to menu

--- src/app/Balloon.App.IntelligentCollection/lib/lib.js
+++ src/app/Balloon.App.IntelligentCollection/lib/lib.js
@@ -31,9 +31,13 @@ var app = {
   preInit: function(core) {
     this.balloon = core;
 
-    this.balloon.addNew('intelligentCollection', 'app.intelligentCollection.addNew', 'folder-filter', this.addIntelligentCollection.bind(this));
+    this.balloon.addNew('intelligentCollection', 'app.intelligentCollection.intelligentCollection', 'folder-filter', this.addIntelligentCollection.bind(this));
 
     this.balloon.toggle_fs_browser_action_hooks['app.intelligentCollection'] = this._fsBroserActionHook;
+
+    app.balloon.addMenu('intelligentCollection', 'app.intelligentCollection.intelligentCollection', 'folder-filter', function() {
+      return app.balloon.refreshTree('/nodes', {query: {filter: {$type: 2}}}, {});
+    });
   },
 
   postInit: function(core)  {
