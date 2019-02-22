gyselroth/balloon-client-web

commit 7c408bf17d7dd64345b7759512463d64c153ae22

BalloonWindow key events
Resolves: #67

src/lib/widget-balloon-window.js
@@ -81,6 +81,15 @@ import iconsSvg from '@gyselroth/icon-collection/src/icons.svg';
         this._object(modals.last())._overlay(true);
       }
     },
+
+    _keydown: function(e) {
+      if(this.options.draggable && !e.ctrlKey && !e.altKey && [37,38,39,40].indexOf(e.keyCode) > -1) {
+        //do not move window with arrow keys (needed for scrolling)
+        return true;
+      }
+
+      Window.fn._keydown.call(this, e);
+    },
   });
 
   ui.plugin(BalloonWindow);

commit de649c8db08437957b8fef7adb21d074707b0cfc

Fix regression introduced with 7c408bf
If a modal has not been opened immediately after creating a BalloonWindow,
the backdrop overlay was shown nevertheless.

src/lib/widget-balloon-window.js
@@ -12,8 +12,8 @@ import iconsSvg from '@gyselroth/icon-collection/src/icons.svg';
       var that = this;
       Window.fn.init.call(this, target, options);
 
-      if(options.closeViaOverlay === undefined) {
-        options.closeViaOverlay = true;
+      if(this.options.closeViaOverlay === undefined) {
+        this.options.closeViaOverlay = true;
       }
 
       var $target = $(target);
@@ -32,9 +32,14 @@ import iconsSvg from '@gyselroth/icon-collection/src/icons.svg';
       }
 
       this.title(options.title);
+    },
 
-      var overlay = options.modal ? this._overlay(true) : $(undefined);
-      if(options.modal && options.closeViaOverlay) {
+    open: function() {
+      var that = this;
+      Window.fn.open.call(this);
+
+      var overlay = this.options.modal ? this._overlay(true) : $(undefined);
+      if(this.options.modal && this.options.closeViaOverlay) {
         overlay.addClass('bln-overlay-clickable');
         overlay.off('click').on('click', function() {
           that.close();