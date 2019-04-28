gyselroth/balloon-client-web

commit c0b230363fb9a95ce941fa204fb4ad26e3c9ea6d

fixes #147

src/lib/app.js
@@ -14,46 +14,57 @@ import burl from '../app/Balloon.App.Burl/lib/lib.js';
 import external from '../app/Balloon.App.ExternalStorage/lib/lib.js';
 import intelligentCollection from '../app/Balloon.App.IntelligentCollection/lib/lib.js';
 
-var apps = [
-  office,
-  convert,
-  notification,
-  clamav,
-  desktop,
-  burl,
-  external,
-  intelligentCollection
-]
+const map = {
+  'Balloon.App.Office': office,
+  'Balloon.App.Convert': convert,
+  'Balloon.App.Notification': notification,
+  'Balloon.App.ClamAv': clamav,
+  'Balloon.App.DesktopClient': desktop,
+  'Balloon.App.Burl': burl,
+  'Balloon.App.ExternalStorage': external,
+  'Balloon.App.IntelligentCollection': intelligentCollection,
+};
 
 var app = {
-  isInstalled: function(appId) {
-    var foundApp = apps.find(function(app) {
-      return app.id !== undefined && app.id === appId;
-    });
+  apps: {
+    'Balloon.App.Office': {enabled: true, config: {}},
+    'Balloon.App.Convert': {enabled: true, config: {}},
+    'Balloon.App.Notification': {enabled: true, config: {}},
+    'Balloon.App.ClamAv': {enabled: true, config: {}},
+    'Balloon.App.DesktopClient': {enabled: true, config: {}},
+    'Balloon.App.Burl': {enabled: true, config: {}},
+    'Balloon.App.ExternalStorage': {enabled: true, config: {}},
+    'Balloon.App.IntelligentCollection': {enabled: true, config: {}}
+  },
 
-    return foundApp !== undefined;
+  init: function(config) {
+    this.apps = $.extend(this.apps, config.apps || {});
+  },
+
+  isEnabled: function(app) {
+    return this.apps[app] && this.apps[app].enabled === true;
   },
 
   render: function() {
-    for(let app in apps) {
-      if(apps[app]['render']) {
-        apps[app].render();
+    for (const [name, app] of Object.entries(this.apps)) {
+      if(app.enabled === true && map[name]['render']) {
+        map[name].render();
       }
     }
   },
 
   preInit: function(core) {
-    for(let app in apps) {
-      if(apps[app]['preInit']) {
-        apps[app].preInit(core);
+    for (const [name, app] of Object.entries(this.apps)) {
+      if(app.enabled === true && map[name]['preInit']) {
+        map[name].preInit(core);
       }
     }
   },
 
   postInit: function(core) {
-    for(let app in apps) {
-      if(apps[app]['postInit']) {
-        apps[app].postInit(core);
+    for (const [name, app] of Object.entries(this.apps)) {
+      if(app.enabled === true && map[name]['postInit']) {
+        map[name].postInit(core);
       }
     }
   }

src/lib/core.js
@@ -5936,7 +5936,7 @@ var balloon = {
    * @return void
    */
   initShareLinkMessageForm: function(node) {
-    if(app.isInstalled('Balloon.App.Notification') === false) {
+    if(app.isEnabled('Balloon.App.Notification') === false) {
       // require the Notification app to be installed in order to send messages
       $fs_share_link_message_form.hide();
       return;

src/lib/translate.js
@@ -121,6 +121,7 @@ var translate = {
 
         $('[data-i18n]').localize();
 
+        app.init(config);
         app.render();
         login.init(translate.config);
 

commit 5e41b8268c9c35ebb5afa0d6cad235b840a26346

Fix uncaught error in IE11 #155
Resolves #155

Fixes regression introduced with: c0b2303

src/lib/app.js
@@ -46,7 +46,11 @@ var app = {
   },
 
   render: function() {
-    for (const [name, app] of Object.entries(this.apps)) {
+    var keys = Object.keys(this.apps);
+    for(var i=0; i<keys.length; i++) {
+      var name = keys[i];
+      var app = this.apps[name];
+
       if(app.enabled === true && map[name]['render']) {
         map[name].render();
       }
@@ -54,7 +58,11 @@ var app = {
   },
 
   preInit: function(core) {
-    for (const [name, app] of Object.entries(this.apps)) {
+    var keys = Object.keys(this.apps);
+    for(var i=0; i<keys.length; i++) {
+      var name = keys[i];
+      var app = this.apps[name];
+
       if(app.enabled === true && map[name]['preInit']) {
         map[name].preInit(core);
       }
@@ -62,7 +70,11 @@ var app = {
   },
 
   postInit: function(core) {
-    for (const [name, app] of Object.entries(this.apps)) {
+    var keys = Object.keys(this.apps);
+    for(var i=0; i<keys.length; i++) {
+      var name = keys[i];
+      var app = this.apps[name];
+
       if(app.enabled === true && map[name]['postInit']) {
         map[name].postInit(core);
       }