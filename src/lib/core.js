gyselroth/balloon-client-web

commit fbfbc33e43eb6e97fcaf0752c830f9824edc498f

Size and changed on second row for smaller screens

src/lib/core.js
@@ -1478,13 +1478,7 @@ var balloon = {
           break;
 
         case 'size':
-          var size = '';
-          if(node.directory) {
-            size = i18next.t('view.prop.data.childcount', {count: node.size})
-          } else {
-            size = balloon.getReadableFileSizeString(node.size || 0);
-          }
-
+          var size = balloon.nodeSize(node);
           $node_el.append('<div class="fs-browser-column fs-browser-column-size">'+size+'</div>');
           break;
 
@@ -1539,6 +1533,12 @@ var balloon = {
             $name_el = $('<div class="fs-browser-column fs-browser-column-name"><span class="fs-name">'+node.name+'</span></div>');
           }
 
+          $name_el.append($(
+            '<p class="fs-browser-column-name-size-changed">' +
+              balloon.nodeSize(node) + ', ' + balloon.timeSince(new Date(node.changed), true)+
+            '</p>'
+          ));
+
           if(balloon.isSearch() && balloon.id(node) !== '_FOLDERUP') {
             var path = node.path.split('/').slice(1, -1);
             if(path.length === 0) path = [''];
@@ -1548,7 +1548,7 @@ var balloon = {
               path.unshift('...');
             }
 
-            var $path_el = $('<p></p>');
+            var $path_el = $('<p class="fs-browser-column-name-path"></p>');
 
             path.forEach(function(item) {
               $path_el.append('<span> / </span><span>' + item + '</span>');
@@ -7562,12 +7562,29 @@ var balloon = {
 
 
   /**
+   * Get a nodes size as a human readable string
+   *
+   * @param   object node
+   * @return  string
+   */
+  nodeSize: function(node) {
+    var size = '';
+    if(node.directory) {
+      size = i18next.t('view.prop.data.childcount', {count: node.size})
+    } else {
+      size = balloon.getReadableFileSizeString(node.size || 0);
+    }
+
+    return size;
+  },
+
+  /**
    * Get time since
    *
    * @param   Date date
    * @return  string
    */
-  timeSince: function(date) {
+  timeSince: function(date, includeAgo) {
     var seconds = Math.floor((new Date() - date) / 1000);
 
     if(seconds < -1) {
@@ -7577,23 +7594,23 @@ var balloon = {
     var interval = Math.floor(seconds / 31536000);
 
     if (interval >= 1) {
-      return i18next.t('time.year', {count: interval});
+      return i18next.t(includeAgo ? 'time.year_ago' : 'time.year', {count: interval});
     }
     interval = Math.floor(seconds / 2592000);
     if (interval >= 1) {
-      return i18next.t('time.month', {count: interval});
+      return i18next.t(includeAgo ? 'time.month_ago' : 'time.month', {count: interval});
     }
     interval = Math.floor(seconds / 86400);
     if (interval >= 1) {
-      return i18next.t('time.day', {count: interval});
+      return i18next.t(includeAgo ? 'time.day_ago' : 'time.day', {count: interval});
     }
     interval = Math.floor(seconds / 3600);
     if (interval >= 1) {
-      return i18next.t('time.hour', {count: interval});
+      return i18next.t(includeAgo ? 'time.hour_ago' : 'time.hour', {count: interval});
     }
     interval = Math.floor(seconds / 60);
     if (interval >= 1) {
-      return i18next.t('time.minute', {count: interval});
+      return i18next.t(includeAgo ? 'time.minute_ago' : 'time.minute', {count: interval});
     }
 
     seconds = Math.round(seconds);

src/themes/default/scss/fsLayoutLeft/_fsBrowser.scss
@@ -1,9 +1,9 @@
 #fs-browser {
   $headerHeight: 28px; //height of the header
   $headerBorderColor: $colorLightGrey;
-  $nodeHeight: 50px; //height of a single node
+  $nodeHeight: 54px; //height of a single node
 
-  $lineHeight: 30px;
+  $lineHeight: 46px;
 
   $iconWidth: 26px;
   $metaWidth: 120px;
@@ -15,9 +15,8 @@
 
   $border-radius: 4px;
 
-  $breakpointMetaVisible: 659px;
-  $breakpointSizeVisible: 720px;
-  $breakpointChangedVisible: 875px;
+  $breakpointMetaVisible: 414px;
+  $breakpointSizeChangedVisible: 750px;
 
   $fsContentHeightMultiselectS: 144px;
   $fsContentHeightSelectS: 200px;
@@ -67,7 +66,7 @@
     width: $iconWidth;
     height: $lineHeight;
     margin: 0;
-    padding: 3px 0;
+    padding: 7px 0;
   }
 
   .fs-browser-column-changed {
@@ -96,15 +95,22 @@
     position: relative;
 
     .fs-browser-column {
-      $paddingTopBottom: ($headerHeight - $lineHeight) / 2;
+      $paddingTopBottom: 0;
 
       position: relative;
+      line-height: 30px;
+      height: 30px;
       padding-top: $paddingTopBottom;
       padding-bottom: $paddingTopBottom;
+
       div {
         display: inline-block;
         width: auto;
       }
+    }
+
+    .fs-browser-column-icon {
+      padding-top: 2px;
     }
 
     .fs-browser-column-name {
@@ -122,7 +128,7 @@
       cursor: pointer;
       display: inline-block;
 
-      padding: 15px 16px 15px 15px;
+      padding: 5px 16px 5px 15px;
 
       span {
         display: block;
@@ -249,12 +255,24 @@
         .fs-browser-column-name {
           width: calc(100% - #{$iconWidth + $checkboxWidth});
           height: $lineHeight;
+          line-height: 17px;
           color: $colorDarkGrey;
           font-size: $fontSizeParagraph;
           font-weight: $fontWeightParagraph;
           text-overflow: ellipsis;
           white-space: nowrap;
           overflow: hidden;
+
+          .fs-name {
+            margin-top: 5px;
+            display: inline-block;
+          }
+
+          & > p {
+            line-height: 14px;
+            margin-top: 2px;
+            color: $colorMediumGrey;
+          }
 
           .fs-ext {
             text-transform: uppercase;
@@ -268,6 +286,10 @@
             margin-top: 0;
             margin-left: -7px;
             border: 1px solid $colorLightGrey;
+          }
+
+          .fs-browser-column-name-path {
+            display: none;
           }
         }
 
@@ -290,6 +312,10 @@
 
           .fs-node-state {
             height: 24px;
+          }
+
+          .gr-icon {
+            display: block;
           }
 
           .fs-color-tag {
@@ -316,20 +342,6 @@
           }
         }
 
-        &.fs-browser-search-item {
-          .fs-browser-column-name {
-            height: 33px;
-            margin-top: -2px;
-            margin-bottom: -1px;
-            line-height: 17px;
-
-            & > p {
-              line-height: 14px;
-              margin-top: 2px;
-              color: $colorMediumGrey;
-            }
-          }
-        }
       }
 
       &:hover {
@@ -464,63 +476,6 @@
     }
   }
 
-  @media screen and (min-width: $breakpointSizeVisible) {
-    #fs-browser-header {
-      .fs-browser-column-size {
-        display: inline-block;
-      }
-
-      .fs-browser-column-name {
-        width: calc(100% - #{$iconWidth + $sizeWidth + $checkboxWidth + 2px});
-      }
-    }
-
-    #fs-browser-tree {
-      .k-item {
-        .k-in {
-          .fs-browser-column-meta {
-            padding-right: 0;
-            width: $metaWidth;
-          }
-
-          .fs-browser-column-size {
-            display: inline-block;
-          }
-
-          .fs-browser-column-name {
-            width: calc(100% - #{$iconWidth + $metaWidth + $sizeWidth + $checkboxWidth});
-          }
-        }
-      }
-    }
-  }
-
-  @media screen and (min-width: $breakpointChangedVisible) {
-    #fs-browser-header {
-      .fs-browser-column-changed {
-        display: inline-block;
-      }
-
-      .fs-browser-column-name {
-        width: calc(100% - #{$iconWidth + $changedWidth + $sizeWidth + $checkboxWidth + 2px});
-      }
-    }
-
-    #fs-browser-tree {
-      .k-item {
-        .k-in {
-          .fs-browser-column-changed {
-            display: inline-block;
-          }
-
-          .fs-browser-column-name {
-            width: calc(100% - #{$iconWidth + $metaWidth + $changedWidth + $sizeWidth + $checkboxWidth});
-          }
-        }
-      }
-    }
-  }
-
   @media screen and (min-width: $breakpointPannelVisible) {
     padding-bottom: #{$bottomPadding} !important;
 
@@ -557,7 +512,7 @@
     }
   }
 
-  @media screen and (min-width: #{$breakpointMetaVisible + $pannelWidthM}) {
+  @media screen and (min-width: #{$breakpointMetaVisible + $pannelWidthM + 100px}) {
     #fs-browser-tree {
       $metaPaddingSmallScreen: 25px;
 
@@ -577,39 +532,11 @@
     }
   }
 
-  @media screen and (min-width: #{$breakpointSizeVisible + $pannelWidthM}) {
+  @media screen and (min-width: #{$breakpointSizeChangedVisible + $pannelWidthM + 100px}) {
+//TODO pixtron - switch here from two lines to one
+
     #fs-browser-header {
-      .fs-browser-column-size {
-        display: inline-block;
-      }
-
-      .fs-browser-column-name {
-        width: calc(100% - #{$iconWidth + $sizeWidth + $checkboxWidth + 2px});
-      }
-    }
-
-    #fs-browser-tree {
-      .k-item {
-        .k-in {
-          .fs-browser-column-meta {
-            padding-right: 0;
-            width: $metaWidth;
-          }
-
-          .fs-browser-column-size {
-            display: inline-block;
-          }
-
-          .fs-browser-column-name {
-            width: calc(100% - #{$iconWidth + $metaWidth + $sizeWidth + $checkboxWidth});
-          }
-        }
-      }
-    }
-  }
-
-  @media screen and (min-width: #{$breakpointChangedVisible + $pannelWidthM}) {
-    #fs-browser-header {
+      .fs-browser-column-size,
       .fs-browser-column-changed {
         display: inline-block;
       }
@@ -622,12 +549,48 @@
     #fs-browser-tree {
       .k-item {
         .k-in {
+          .fs-browser-column-meta {
+            padding-right: 0;
+            width: $metaWidth;
+          }
+
+          .fs-browser-column-size,
           .fs-browser-column-changed {
             display: inline-block;
           }
 
           .fs-browser-column-name {
             width: calc(100% - #{$iconWidth + $metaWidth + $changedWidth + $sizeWidth + $checkboxWidth});
+
+            line-height: $lineHeight;
+
+            .fs-name {
+              margin-top: 0;
+            }
+
+            .fs-browser-column-name-size-changed {
+              display: none !important;
+            }
+
+            .fs-browser-column-name-path {
+              display: block;
+            }
+          }
+
+          &.fs-browser-search-item {
+            .fs-browser-column-name {
+              line-height: 17px;
+
+              .fs-name {
+                margin-top: 5px;
+              }
+
+              & > p {
+                line-height: 14px;
+                margin-top: 2px;
+                color: $colorMediumGrey;
+              }
+            }
           }
         }
       }

commit 2248cb6dd128a878aa3b567b886718143220a1d6

Fix _FOLDERUP
Fixes regression introduced with fbfbc33

src/lib/core.js
@@ -1537,11 +1537,13 @@ var balloon = {
             $name_el = $('<div class="fs-browser-column fs-browser-column-name"><span class="fs-name">'+node.name+'</span></div>');
           }
 
-          $name_el.append($(
-            '<p class="fs-browser-column-name-size-changed">' +
+          if(balloon.id(node) !== '_FOLDERUP') {
+            $name_el.append($(
+              '<p class="fs-browser-column-name-size-changed">' +
               balloon.nodeSize(node) + ', ' + balloon.timeSince(new Date(node.changed), true)+
-            '</p>'
-          ));
+              '</p>'
+            ));
+          }
 
           if(balloon.isSearch() && balloon.id(node) !== '_FOLDERUP') {
             var path = node.path.split('/').slice(1, -1);

src/themes/default/scss/fsLayoutLeft/_fsBrowser.scss
@@ -429,17 +429,6 @@
 
       &.fs-folderup {
         display: none;
-        margin-bottom: 20px;
-
-        .k-in {
-          .fs-browser-column-icon {
-            color: $colorActiveBlue;
-          }
-
-          .fs-browser-column-name {
-            color: $colorMediumGrey;
-          }
-        }
       }
     }
   }
@@ -477,6 +466,22 @@
       .k-item {
         &.fs-folderup {
           display: block;
+          margin-bottom: 20px;
+
+          .k-in {
+            .fs-browser-column-icon {
+              color: $colorActiveBlue;
+            }
+
+            .fs-browser-column-name {
+              color: $colorMediumGrey;
+              line-height: $lineHeight;
+
+              .fs-name {
+                margin-top: 0;
+              }
+            }
+          }
         }
       }
     }