4e4abd9c3ef1ab3d09a7365f238604e04c24bded
Stefan Aebischer <aebischer@pixtron.ch>
2019-05-12 21:38:02 +0800

    Upgrade @openid/appauth to v1.2.3

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -343,8 +343,16 @@ var login = {
     localStorage.lastIdpUrl = provider_url;
 
     AuthorizationServiceConfiguration.fetchFromIssuer(idp.providerUrl).then(configuration => {
-      var request = new AuthorizationRequest(
-        idp.clientId, idp.redirectUri, idp.scope, 'id_token token', undefined, {'nonce': Math.random().toString(36).slice(2)});
+      var config = {
+        client_id: idp.clientId,
+        redirect_uri: idp.redirectUri,
+        scope: idp.scope,
+        response_type: 'id_token token',
+        state: undefined,
+        extras: {nonce: Math.random().toString(36).slice(2)}
+      }
+
+      var request = new AuthorizationRequest(config);
 
       login.handler.performAuthorizationRequest(configuration, request);
     });
