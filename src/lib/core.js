gyselroth/balloon-client-web

commit 61d1b018b753109d371d245523a7c45d79215477

reloadTree should reload search result
Resolves: #173

src/lib/core.js
@@ -164,8 +164,7 @@ var balloon = {
         return balloon._buildQueryNodename(value, filters);
       },
       executeQuery: function(query) {
-        balloon.refreshTree('/nodes', {query: query});
-        return true;
+        return balloon.refreshTree('/nodes', {query: query});
       }
     }
   },
@@ -1923,12 +1922,9 @@ var balloon = {
     var collection =  balloon.getCurrentCollectionId();
     var node = balloon.getCurrentNode();
 
-    if(balloon.isSearch() && balloon.getCurrentCollectionId() === null) {
-      //Do not reload tree - if in search mode
-      return $.Deferred().resolve().promise();
-    }
-
-    if(!collection) {
+    if(balloon.isSearch() && collection === null) {
+      return balloon.buildExtendedSearchQuery();
+    } else if(!collection) {
       return balloon.menuLeftAction(menu, true).then(function() {
         if(node) {
           balloon.last = node;
@@ -6569,9 +6565,12 @@ var balloon = {
 
     var query = balloon.buildQuery(content, filters);
 
-    if(query === undefined || !balloon.executeQuery(query)) {
+    if(query === undefined) {
       balloon.datasource.data([]);
+      return;
     }
+
+    return balloon.executeQuery(query);
   },
 
 
@@ -6603,9 +6602,11 @@ var balloon = {
 
     if(balloon.search_modes && balloon.search_modes[mode] && balloon.search_modes[mode].executeQuery) {
       return balloon.search_modes[mode].executeQuery(query);
+    } else {
+      balloon.datasource.data([]);
+      return $.Deferred().resolve().promise();
     }
 
-    return false;
   },
 
   /**

src/app/Balloon.App.Elasticsearch/lib/lib.js
@@ -34,8 +34,7 @@ var app = {
    * @return  boolean
    */
   executeQuery: function(query) {
-    app.balloon.refreshTree('/files/search', {query: query});
-    return true;
+    return app.balloon.refreshTree('/files/search', {query: query});
   },
 
   /**

commit 7a183f0a63d3ef914be53b4caef8762baa9e3318

Fixes regression introduced with 61d1b01
balloon.isSearch() returns true for any menu except cloud,
therefore it is not suitable to decide, if tree should be reloaded with
last search result or current menu

Related to: #173

src/lib/core.js
@@ -1926,7 +1926,7 @@ var balloon = {
     var collection =  balloon.getCurrentCollectionId();
     var node = balloon.getCurrentNode();
 
-    if(balloon.isSearch() && collection === null) {
+    if(balloon.isSearchResult() && collection === null) {
       return balloon.buildExtendedSearchQuery();
     } else if(!collection) {
       return balloon.menuLeftAction(menu, true).then(function() {
@@ -6681,12 +6681,22 @@ var balloon = {
   },
 
   /**
-   * Check if search window is active
+   * Check if search breadcrumb is visible
    *
    * @return bool
    */
   isSearch: function() {
     return $('#fs-crumb-search-list').is(':visible');
+  },
+
+
+  /**
+   * Check if tree displays a search result
+   *
+   * @return bool
+   */
+  isSearchResult: function() {
+    return $('#fs-crumb-search').data('is-search-result') === true;
   },
 
 