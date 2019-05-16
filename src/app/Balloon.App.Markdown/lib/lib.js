01f883ab91454cfc9c71ec68527164d5443f2f6d
Stefan Aebischer <aebischer@pixtron.ch>
2019-05-16 15:14:18 +0800

    Disable spellchecker

--- src/app/Balloon.App.Markdown/lib/lib.js
+++ src/app/Balloon.App.Markdown/lib/lib.js
@@ -145,6 +145,7 @@ var app = {
       element: this.$windowHtml.find('textarea')[0],
       autofocus: true,
       toolbar: ['bold', 'italic', '|', 'heading-1', 'heading-2', 'heading-3', '|', 'code', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview', '|', 'guide'],
+      spellChecker: false,
       shortcuts: {
         'toggleSideBySide': null,
         'toggleFullScreen': null,
