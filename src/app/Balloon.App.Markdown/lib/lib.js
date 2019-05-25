gyselroth/balloon-client-web

commit 1fd2300246f4577451c4bd89946a7a24feb26007

fullscreen window #179

src/app/Balloon.App.Markdown/lib/lib.js
@@ -12,7 +12,6 @@ import marked from 'marked';
 import balloonWindow from '../../../lib/widget-balloon-window.js';
 import css from '../styles/style.scss';
 
-
 var app = {
   id: 'Balloon.App.Markdown',
 
@@ -77,8 +76,9 @@ var app = {
         app.editor.k_window = app.$windowHtml.kendoBalloonWindow({
           title: winTitle,
           resizable: false,
-          modal: true,
+          modal: false,
           draggable: false,
+          fullscreen: true,
           close: function(e) {
             if(e.userTriggered && app._editorCancel() === false) {
               e.preventDefault();
@@ -93,7 +93,7 @@ var app = {
               app._togglePreview(false);
             }
           }
-        }).data("kendoBalloonWindow").center().maximize();
+        }).data("kendoBalloonWindow").center();
       }
     });
 
@@ -170,6 +170,17 @@ var app = {
         'toggleFullScreen': null,
       },
     });
+
+    var parent_node = app.balloon.getCurrentCollectionId();
+    var $fs_browser_tree = $('#fs-browser-tree');
+     this.$windowHtml.unbind('drop').on('drop', function(e) {
+       console.log(e);
+       $fs_browser_tree.removeClass('fs-file-dropable');
+       $fs_browser_tree.find('.fs-file-drop').removeClass('fs-file-drop');
+       $('#fs-upload').removeClass('fs-file-dropable');
+
+       app.balloon._handleFileSelect(e, parent_node);
+     });
   },
 
   /**
@@ -182,6 +193,7 @@ var app = {
 
     var renderer = new marked.Renderer();
     var linkRenderer = renderer.link;
+    var imageRenderer = renderer.image;
 
     renderer.link = function(href, title, text) {
       var html = linkRenderer.call(renderer, href, title, text);
@@ -190,6 +202,16 @@ var app = {
       } else {
         return html;
       }
+    }
+
+    renderer.image = function(href, title, text) {
+      var html = imageRenderer.call(renderer, href, title, text);
+      var url = app.balloon.base+'/files/content?id='+href;
+      if(typeof(app.balloon.login) === 'object' && app.balloon.login.getAccessToken()) {
+        url += '&access_token='+app.balloon.login.getAccessToken();
+      }
+
+      return '<img alt="'+title+'" src="'+url+'"/>';
     }
 
     markedOptions.renderer = renderer;
@@ -226,7 +248,7 @@ var app = {
     });
 
     $("#fs-prompt-window").find('input[name=cancel]').unbind('click').bind('click', function(){
-      $("#fs-prompt-window").data('kendoBalloonWindow').close();
+      $("#fs-prompt-window").data('kendoBalloonFullscreenWindow').close();
       app._closeEditorWindow();
     });
 

src/app/Balloon.App.Office/lib/lib.js
@@ -93,6 +93,7 @@ var app = {
       title: node.name,
       modal: false,
       draggable: false,
+      fullscreen: true,
       keydown: function(e) {
         if(e.originalEvent.keyCode !== 27) {
           return;
@@ -135,7 +136,7 @@ var app = {
           });
         });
       }
-    }).data("kendoBalloonWindow").center().maximize();
+    }).data("kendoBalloonWindow").center();
   },
 
   eventHandler: function(node) {

src/index.html
@@ -1097,6 +1097,8 @@
             </div>
         </div>
 
+        <div id="fs-preview-window"></div>
+
         <div id="fs-new-node-window">
             <div class="fs-window-form">
                 <label for="fs-new-node-window-name" data-i18n="new_node.name"></label>

src/lib/core.js
@@ -777,6 +777,18 @@ var balloon = {
       handler: balloon.displayFile
     });
 
+    balloon.addFileHandler({
+      app: 'balloon preview viewer',
+      test: function(node){return node.preview},
+      handler: balloon.openPreview
+    });
+
+    balloon.addFileHandler({
+      app: 'balloon file downloader',
+      test: function(node){return true;},
+      handler: balloon.downloadNode
+    });
+
     balloon.showHint()
 
     pullToRefresh({
@@ -7432,7 +7444,8 @@ var balloon = {
         var $k_display = $div.kendoBalloonWindow({
           title: winTitle,
           resizable: false,
-          modal: true,
+          modal: false,
+          fullscreen: true,
           draggable: false,
           keydown: function(e) {
             if(e.originalEvent.keyCode !== 27) {
@@ -7469,7 +7482,7 @@ var balloon = {
               data = $textarea.val();
             });
           }
-        }).data("kendoBalloonWindow").center().open().maximize();
+        }).data("kendoBalloonWindow").center().open();
       }
     });
 
@@ -8301,6 +8314,22 @@ var balloon = {
     });
   },
 
+  /**
+   * Open preview
+   */
+  openPreview: function(node) {
+    var $fs_preview_win = $('#fs-preview-window');
+    var $preview = $('#fs-preview-thumb').find('img');
+
+    var $k_preview_win = $fs_preview_win.kendoBalloonWindow({
+      title: node.name,
+      resizable: false,
+      modal: true,
+      open: function() {
+        $fs_preview_win.html($preview);
+      }
+    }).data('kendoBalloonWindow').center().open();
+  },
 
   /**
    * Display preview
@@ -9750,7 +9779,7 @@ var balloon = {
 
         var data = balloon.parseError(e);
         if(data === false || data.status != 403) {
-          balloon.displayError(response);
+          balloon.displayError(e);
         } else {
           if(data.code === 40) {
             var new_name = balloon.getCloneName(file.blob.name);

src/lib/widget-balloon-window.js
@@ -31,6 +31,10 @@ import iconsSvg from '@gyselroth/icon-collection/src/icons.svg';
         }
       }
 
+      if(options.fullscreen === true) {
+        $parent.addClass('fs-fullscreen-window');
+      }
+
       this.title(options.title);
     },
 

src/main.js
@@ -5,13 +5,13 @@
  * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
  */
 
+import "@babel/polyfill";
 import iconsSvg from '@gyselroth/icon-collection/src/icons.svg';
 import $ from "jquery";
 import translate from './lib/translate.js';
 import svgxuse from 'svgxuse';
 import balloonCss from './themes/default/scss/balloon.scss';
 import { polyfill } from 'es6-promise'; polyfill();
-
 window.jquery = $;
 
 $.ajax({

src/themes/default/scss/modals/_base.scss
@@ -1,3 +1,11 @@
+.fs-fullscreen-window {
+  left: 0 !important;
+  top: 0 !important;
+  right: 401px !important;
+  height: 100% !important;
+  border-radius: 0 !important;
+}
+
 .k-window {
   $headerHeight: 72px;
   $sidePadding: 60px;

src/themes/default/scss/modals/_fsEditLive.scss
@@ -1,11 +1,5 @@
 $sidePadding: 60px;
 
-.fs-edit-live {
-  width: 70%;
-  min-height: 70%;
-  max-height: 100vh;
-}
-
 #fs-edit-live {
   display: none;
 
@@ -13,5 +7,4 @@ $sidePadding: 60px;
     height: calc(100%  - 100px);
     min-height: 300px;
   }
-
 }

commit e3de3fc9305023f3be7e6c8b6164ce5bb003e5d5

Fix regression introduced with 1fd2300
Cancel resulted in:
Uncaught TypeError: Cannot read property 'close' of undefined

src/app/Balloon.App.Markdown/lib/lib.js
@@ -248,7 +248,7 @@ var app = {
     });
 
     $("#fs-prompt-window").find('input[name=cancel]').unbind('click').bind('click', function(){
-      $("#fs-prompt-window").data('kendoBalloonFullscreenWindow').close();
+      $("#fs-prompt-window").data('kendoBalloonWindow').close();
       app._closeEditorWindow();
     });
 