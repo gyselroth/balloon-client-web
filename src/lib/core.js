gyselroth/balloon-client-web

commit 0303f5ecc98179499e182a7e187be47542804194

finalized #149

src/lib/core.js
@@ -1886,7 +1886,6 @@ var balloon = {
     }, 1000);
   },
 
-
   /**
    * Build breadcrumb with parents
    *
@@ -5213,9 +5212,9 @@ var balloon = {
    *
    * @return bool
    */
-  showShare: function() {
+  showShare: function(node) {
     var acl = [];
-    var node = balloon.getCurrentNode();
+    node = node || balloon.getCurrentNode();
 
     if(!node || !node.directory) return;
 

src/app/Balloon.App.Office/lib/lib.js
@@ -110,20 +110,21 @@ var app = {
           $('#fs-browser-tree').find('li[gr-id="'+node.id+'"]').find('.k-in').find('> span').clone()
         );
 
-        //var src = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port+app.balloon.base+'/office/wopi/files/'+session.file;
-        var src = window.location.protocol + '//' + '10.242.2.13' + ':' + '8084'+app.balloon.base+'/office/wopi/files/'+session.node;
+        var src = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port+app.balloon.base+'/office/wopi/files/'+session.file;
+        //var src = window.location.protocol + '//' + '10.242.2.8' + ':' + '8084'+app.balloon.base+'/office/wopi/files/'+session.node;
         src = encodeURIComponent(src);
         var url = app.parseUrl(context.url, src, node);
 
         $div.append(
-          '<form method="post" action="'+url+'" target="loleafletframe">'+
+          '<form method="post" action="'+url+'" target="office">'+
              '<input type="hidden" name="access_token" value="'+session.access_token+'"/>'+
              '<input type="hidden" name="access_token_ttl" value="'+session.ttl+'"/>'+
           '</form>'+
-          '<iframe style="width: 100%; height: 100%;" name="loleafletframe"/>'
+          '<iframe style="width: 100%; height: 100%;" name="office"/>'
         );
 
         $div.find('form').submit();
+        app.eventHandler(node);
 
         $(this.wrapper).find('.k-i-close').unbind('click.fix').bind('click.fix', function(e){
           e.stopImmediatePropagation();
@@ -135,6 +136,34 @@ var app = {
         });
       }
     }).data("kendoBalloonWindow").center().maximize();
+  },
+
+  eventHandler: function(node) {
+    $(window).off('message').on('message', function(e) {
+      var msg = JSON.parse(e.originalEvent.data);
+      var msgId = msg.MessageId;
+      var msgData = msg.Values;
+
+      switch(msgId) {
+        case 'File_Rename':
+          $('#fs-edit-office_wnd_title').html(msgData.NewName+'.'+app.balloon.getFileExtension(node));
+        break;
+
+        case 'UI_FileVersions':
+          app.balloon.displayHistoryWindow(node)
+        break;
+
+        case 'UI_Sharing':
+          app.balloon.xmlHttpRequest({
+            url: app.balloon.base+'/nodes/'+app.balloon.getCurrentCollectionId(),
+            type: 'GET',
+            success: function(node) {
+              app.balloon.showShare(node);
+            },
+          });
+        break;
+      }
+    });
   },
 
   parseUrl: function(url, src, node) {

commit df5e6f69245c0cf1360056d0df1e742cf6ba3677

Fix regression introduced with 0303f5e
Resolves: #193

src/lib/core.js
@@ -5182,9 +5182,9 @@ var balloon = {
     var $fs_share_edit = $fs_share.find('#fs-share-edit');
     var $fs_share_delete = $fs_share.find('#fs-share-delete');
 
-    $fs_share_create.off('click').on('click', balloon.showShare);
+    $fs_share_create.off('click').on('click', balloon._onShowShare);
 
-    $fs_share_edit.off('click').on('click', balloon.showShare);
+    $fs_share_edit.off('click').on('click', balloon._onShowShare);
 
     $fs_share_delete.off('click').on('click', function() {
       balloon.deleteShare(node);
@@ -5229,7 +5229,7 @@ var balloon = {
     var $li_owner = $('<li class="avatar-user"></li>');
     $fs_share_consumers_ul.append($li_owner);
 
-    $fs_share_consumers_ul.off('click').on('click', balloon.showShare);
+    $fs_share_consumers_ul.off('click').on('click', balloon._onShowShare);
 
     balloon.displayAvatar($li_owner, login.user.id);
 
@@ -5261,13 +5261,24 @@ var balloon = {
   },
 
   /**
+   * Event handler wrapper for showShare
+   *
+   * @param object event
+   * @return void
+   */
+  _onShowShare: function(event) {
+    var node = balloon.getCurrentNode();
+    balloon.showShare(node);
+  },
+
+  /**
    * Shows popup for share creating/edting
    *
-   * @return bool
+   * @param object node
+   * @return void
    */
   showShare: function(node) {
     var acl = [];
-    node = node || balloon.getCurrentNode();
 
     if(!node || !node.directory) return;
 